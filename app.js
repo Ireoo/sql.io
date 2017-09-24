adc36c26cefd2ee384751af4076e138c0dee7a44
S2 <s@ireoo.com>
2017-09-24 11:01:31 +0800

    update

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
-const sql = () => {
-    this.SQL = [];
+const Sql = () => {
+    this.SQL = [require(`./function/api`)];
+    return this;
 };
 
-sql.prototype.use = (mode) => {
+Sql.prototype.use = (mode) => {
+    try {
+        this.SQL = [require(`./function/${mode}`)];
+    } catch (e) {
+        console.error(e);
+    }
+    return this;
+};
+
+Sql.prototype.add = (mode) => {
     try {
         this.SQL.push(require(`./function/${mode}`));
     } catch (e) {
         console.error(e);
-};
-
-sql.prototype.add = (mode) => {
-    this.SQL.push();
-};
-
-sql.prototype.table = (table) => {
-    this.SQL.table(table);
+    }
     return this;
 };
 
-sql.prototype.
+Sql.prototype.table = (table) => {
+    for (let i in this.SQL) {
+        let Q = this.SQL[i];
+        Q.table(table);
+    }
+    return this;
+};
+
+Sql.prototype.sql = (sql) => {
+    for (let i in this.SQL) {
+        let Q = this.SQL[i];
+        Q.sql(sql);
+    }
+    return this;
+};
+
+Sql.prototype.run = () => {
+    for (let i in this.SQL) {
+        let Q = this.SQL[i];
+        Q.run();
+    }
+    return this;
+};
